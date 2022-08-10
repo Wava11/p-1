da0e1f660c2e2aed42459d8ee030912f33ae2c0f
tomerProg <tomercpc01@gmail.com>
2022-08-10 15:10:51 +0300

    moved avatar to bottom

--- components/menu/styles.tsx
+++ components/menu/styles.tsx
@@ -4,12 +4,15 @@ import { Theme } from '@mui/material';
 export const styles = (theme: Theme) => createStyles({
     root: {
         height: '100%',
+        position: 'relative',
     },
 
     avatar: {
         display: 'flex',
         alignItems: 'center',
-        padding: '5px'
+        padding: '5px',
+        bottom: '20px',
+        position: 'fixed',
     },
 
     username: {
